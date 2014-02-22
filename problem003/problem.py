2a0f9299cb30a88649834fd957d9572cdb7ee8b5
Tom Kelley <distortedsignal@gmail.com>
2014-02-21 23:08:30 -0800

    Commenting out first solution to problem 3, since it's embarrasingly slow.

--- problem003/problem.py
+++ problem003/problem.py
@@ -4,15 +4,15 @@ from math import sqrt, ceil
 
 bigConst = 600851475143
 
-startTime = c()
-#First attempt, not bad, but not optimal (~6.7 seconds)
-for i in xrange(2,bigConst):
-	if bigConst % i == 0:
-		if p(int(bigConst/i)):
-			print int(bigConst/i)
-			break
+# startTime = c()
+# #First attempt, not bad, but not optimal (~6.7 seconds)
+# for i in xrange(2,bigConst):
+# 	if bigConst % i == 0:
+# 		if p(int(bigConst/i)):
+# 			print int(bigConst/i)
+# 			break
 
-print c() - startTime
+# print c() - startTime
 
 startTime = c()
 #Second attempt, better, but still not great (~0.07 seconds)
