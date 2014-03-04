bb668cbd1256eac5bdff330285aea74ec5a3c4fd
Tom Kelley <tkelley@axolotl.com>
2014-03-03 16:55:48 -0800

    Problem 83 not working, but it's freaking fast.

--- problem083/problem.py
+++ problem083/problem.py
@@ -12,4 +12,71 @@ for line in f:
 	# Complexity: I'm going to assume N
 	matrix.append(map(int,line.strip().split(",")))
 
-print matrix
+result = matrix
+
+for i in range(len(matrix) - 1, -1, -1):
+	for j in range(len(matrix[i]) - 1, -1, -1):
+		if i == len(matrix) - 1 and j == len(matrix) - 1:
+			pass
+		elif i == len(matrix) - 1:
+			result[i][j] = matrix[i][j] + result[i][j+1]
+		elif j == len(matrix) - 1:
+			result[i][j] = matrix[i][j] + result[i+1][j]
+		else:
+			result[i][j] = matrix[i][j] + min([result[i+1][j], result[i][j+1]])
+
+rebalanced = True
+
+while rebalanced:
+	rebalanced = False
+
+	for i in range(len(matrix) - 1, -1, -1):
+		for j in range(len(matrix) - 1, -1, -1):
+			if i == len(matrix) - 1 and j == len(matrix) - 1:
+				# End of maze
+				continue
+			if i == 0 and j == 0:
+				# Origin of maze
+				if result[i][j] > matrix[i][j] + min([result[i+1][j], result[i][j+1]]):
+					result[i][j] = matrix[i][j] + min([result[i+1][j], result[i][j+1]])
+					rebalanced = True
+			elif i == len(matrix) - 1 and j == 0:
+				# Lower left corner
+				if result[i][j] > matrix[i][j] + min([result[i-1][j], result[i][j+1]]):
+					result[i][j] = matrix[i][j] + min([result[i-1][j], result[i][j+1]])
+					rebalanced = True
+			elif i == 0 and j == len(matrix) - 1:
+				# Upper right corner
+				if result[i][j] > matrix[i][j] + min([result[i][j-1], result[i+1][j]]):
+					result[i][j] = matrix[i][j] + min([result[i][j-1], result[i+1][j]])
+					rebalanced = True
+			elif i == len(matrix) - 1:
+				# Bottom edge
+				if result[i][j] > matrix[i][j] + min([result[i][j+1], result[i][j-1], result[i-1][j]]):
+					result[i][j] = matrix[i][j] + min([result[i][j+1], result[i][j-1], result[i-1][j]])
+					rebalanced = True
+			elif i == 0:
+				# Top edge
+				if result[i][j] > matrix[i][j] + min([result[i][j+1], result[i][j-1], result[i+1][j]]):
+					result[i][j] = matrix[i][j] + min([result[i][j+1], result[i][j-1], result[i+1][j]])
+					rebalanced = True
+			elif j == len(matrix) - 1:
+				# Right edge
+				if result[i][j] > matrix[i][j] + min([result[i+1][j], result[i-1][j], result[i][j-1]]):
+					result[i][j] = matrix[i][j] + min([result[i+1][j], result[i-1][j], result[i][j-1]])
+					rebalanced = True
+			elif i == len(matrix) - 1:
+				# Left edge
+				if result[i][j] > matrix[i][j] + min([result[i+1][j], result[i-1][j], result[i][j+1]]):
+					result[i][j] = matrix[i][j] + min([result[i+1][j], result[i-1][j], result[i][j+1]])
+					rebalanced = True
+			else:
+				# The creamy middles
+				if result[i][j] > matrix[i][j] + min([result[i+1][j], result[i-1][j], result[i][j+1], result[i][j-1]]):
+					result[i][j] = matrix[i][j] + min([result[i+1][j], result[i-1][j], result[i][j+1], result[i][j-1]])
+					rebalanced = True
+
+
+
+
+print result
