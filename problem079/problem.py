c90ec9e80c57df5c436a7781ecd38cf6ac98e923
Tom Kelley <distortedsignal@gmail.com>
2014-03-06 21:43:35 -0800

    Removing dot so print works well.

--- problem079/problem.py
+++ problem079/problem.py
@@ -4,7 +4,7 @@ key = []
 for line in f:
 	key.append(line.strip())
 
-print key.
+print key
 
 # firstFreq = {'0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0}
 # for a in key:
