41834a50a5de5e9c25b77593f085d234d7712172
Tom Kelley <tkelley@axolotl.com>
2014-03-12 17:12:36 -0700

    Adding problem 144 directory and some initial thoughts on what the code for this should look like. DO NOT RUN THIS CODE. IT WILL NOT WORK.

---
+++ problem144/problem.py
@@ -0,0 +1,54 @@
+from math import sqrt
+
+def reflect(beamSlope, beamDir, wallSlope):
+	'''Return the slope (as a float) of a reflected beam given the incident 
+		beam slope (as a float), the beam direction (as 1 for moving in the positive x, 0 for 
+		infinite slope, or -1 for moving in the negative x direction), and the 
+		slope of the wall (as a float).'''
+
+	assert type(beamSlope) is float, "beamSlope must be a float."
+	assert type(beamDir) is int and beamDir in [-1.0, 0.0, 1.0], "beamDir must be in [-1, 0, 1]."
+	assert type(wallSlope) is float, "wallSlope must be a float."
+	if beamSlope in [float("inf"), float("-inf")]:
+		assert beamDir == 0.0, "If the beam slope is infinite, the direction should be zero."
+	else:
+		assert beamDir in [1.0, -1.0], "If the beam slope is non-infinate, there must be a " + \
+			"direction associated with the beam."
+
+	wallNormal = 0
+	if wallSlope == 0:
+		wallNormal = float("inf")
+	elif abs(wallSlope) == float("inf"):
+		wallNormal = 0
+	else:
+		wallNormal = -1/wallSlope
+
+
+
+	pass
+
+oldSpot = [0, 10.1]
+newSpot = [1.4, -9.6]
+bounce = 0
+
+while newSpot[0] < -0.01 and newSpot[0] > 0.01 and newSpot[1] < 9:
+	wallSlope = 0
+	beamSlope = 0
+	beamDir = 0
+
+	if newSpot[1] == 0:
+		wallSlope = float("inf")
+	else:
+		wallSlope = -4*x/y
+
+	if newSpot[0] - oldSpot[0] == 0:
+		beamSlope = float("inf") * (float(newSpot[1] - oldSpot[1])/abs(float(newSpot[1] - oldSpot[1])))
+	else:
+		beamSlope = float(newSpot[1] - oldSpot[1])/float(newSpot[0] - oldSpot[0])
+		beamDir = round(float(newSpot[0] - oldSpot[0])/float(abs(newSpot[0] - oldSpot[0])))
+
+	reflectedSlope = reflect()
+	bounce += 1
+	oldSpot = newSpot
+	newSpot = calcNewSpot()
+	break
