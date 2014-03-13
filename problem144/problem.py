0011213fbf324adb336e703ef2e14e63584e4537
Tom Kelley <tkelley@axolotl.com>
2014-03-13 15:44:31 -0700

    I don't know what I changed, I'm just going to assume it's worth it to add it. I don't feel like I'll be doing a lot more on this today.

--- problem144/problem.py
+++ problem144/problem.py
@@ -7,13 +7,14 @@ def reflect(beamSlope, beamDir, wallSlope):
 		infinite slope, or -1 for moving in the negative x direction), and the 
 		slope of the wall (as a float).'''
 
-	assert type(beamSlope) is float, "beamSlope must be a float."
-	assert type(beamDir) is int and beamDir in [-1.0, 0.0, 1.0], "beamDir must be in [-1, 0, 1]."
-	assert type(wallSlope) is float, "wallSlope must be a float."
+	assert type(beamSlope) is float, "beamSlope " + str(beamSlope) + " must be a float."
+	assert type(beamDir) is int and beamDir in [-1, 0, 1], "beamDir " + str(beamDir) + \
+		" must be in [-1, 0, 1]."
+	assert type(wallSlope) is float, "wallSlope" + str(wallSlope) + "must be a float."
 	if beamSlope in [float("inf"), float("-inf")]:
-		assert beamDir == 0.0, "If the beam slope is infinite, the direction should be zero."
+		assert beamDir == 0, "If the beam slope is infinite, the direction should be zero."
 	else:
-		assert beamDir in [1.0, -1.0], "If the beam slope is non-infinate, there must be a " + \
+		assert beamDir in [1, -1], "If the beam slope is non-infinate, there must be a " + \
 			"direction associated with the beam."
 
 	wallNormal = 0
@@ -25,13 +26,12 @@ def reflect(beamSlope, beamDir, wallSlope):
 		wallNormal = -1/wallSlope
 	innerAngle = atan((beamSlope - wallNormal)/(1 + (beamSlope*wallNormal)))
 	return tan(atan(wallSlope) - innerAngle)
-	raise NotImplementedError()
 
 oldSpot = [0, 10.1]
 newSpot = [1.4, -9.6]
 bounce = 0
 
-while newSpot[0] < -0.01 and newSpot[0] > 0.01 and newSpot[1] < 9:
+while (newSpot[0] < -0.01 or newSpot[0] > 0.01) and newSpot[1] < 9:
 	wallSlope = 0
 	beamSlope = 0
 	beamDir = 0
@@ -39,12 +39,12 @@ while newSpot[0] < -0.01 and newSpot[0] > 0.01 and newSpot[1] < 9:
 	if newSpot[1] == 0.0:
 		wallSlope = float("inf")
 	else:
-		wallSlope = -4*x/y
+		wallSlope = -4*newSpot[0]/newSpot[1]
 
 	beamSlope = float(newSpot[1] - oldSpot[1])/float(newSpot[0] - oldSpot[0])
 	beamDir = round(float(newSpot[0] - oldSpot[0])/float(abs(newSpot[0] - oldSpot[0])))
 
-	reflectedSlope = reflect(beamSlope, beamDir, wallSlope)
+	reflectedSlope = reflect(beamSlope, int(round(beamDir)), wallSlope)
 	bounce += 1
 	oldSpot = deepcopy(newSpot)
 	newSpot = calcNewSpot()
