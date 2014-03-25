8e4bfd198bbec72343e01255e90dd2b521f3d659
Tom Kelley <tkelley@axolotl.com>
2014-03-25 13:16:57 -0700

    Re-bundling problem 2 into a function to facilitate moving into server later.

--- problem002/problem.py
+++ problem002/problem.py
@@ -1,3 +1,4 @@
-from mathLib.mathLib import fibGen as f
-
-print sum(filter(lambda i: i % 2 == 0, f(4000000)))
+def simpleSolution():
+	from mathLib.generators import fibGen as f
+	return sum(filter(lambda i: i % 2 == 0, f(4000000)))
+	
