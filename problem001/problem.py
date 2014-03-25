b1f8d17b81333a28850bd6b06cb35a6714676baa
Tom Kelley <tkelley@axolotl.com>
2014-03-25 11:31:40 -0700

    Starting to change structure of PE problems so that I can set up a Flask application to display answers on the web.

--- problem001/problem.py
+++ problem001/problem.py
@@ -1,4 +1,5 @@
 # My solution, simple.
-print sum([i for i in range(1000) if i % 3 == 0 or i % 5 == 0])
+def simpleSolution():
+	return sum([i for i in range(1000) if i % 3 == 0 or i % 5 == 0])
 
 #Optimal solution given by project euler
