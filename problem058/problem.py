b32d7b0576f8c9fd55dd1ac4012f56ff08a9bbe8
Tom Kelley <tkelley@axolotl.com>
2014-03-25 14:46:16 -0700

    Making changes like in the last two commits, only for problems 58 and 102.

--- problem058/problem.py
+++ problem058/problem.py
@@ -1,23 +1,28 @@
-from mathLib.mathLib import isPrime
-from time import clock as cl
+def simpleSolution():
+	from mathLib.mathLib import isPrime
+	lastNum = 1
+	sideLen = 1
+	primes = 0
+	while True:
 
-startTime = cl()
-lastNum = 1
-sideLen = 1
-primes = 0
-while True:
+		# Add 2 to sideLen to simulate that we're adding a new layer
+		sideLen += 2
 
-	# Add 2 to sideLen to simulate that we're adding a new layer
-	sideLen += 2
+		# Find corners and test if they are prime
+		for i in range(4):
+			lastNum += sideLen - 1
+			if isPrime(lastNum):
+				primes += 1
 
-	# Find corners and test if they are prime
-	for i in range(4):
-		lastNum += sideLen - 1
-		if isPrime(lastNum):
-			primes += 1
+		# Check if we got there
+		if (float(primes)/float((2*sideLen)-1)) < .1:
+			break
 
-	# Check if we got there
-	if (float(primes)/float((2*sideLen)-1)) < .1:
-		break
+	return sideLen
 
-print sideLen, cl() - startTime
+if __name__ == "__main__":
+	import os, sys, inspect
+	cmd_folder = os.sep.join(os.path.abspath(inspect.getfile(inspect.currentframe())).split(os.sep)[:-2])
+	if cmd_folder not in sys.path:
+		sys.path.insert(0, cmd_folder)
+	print simpleSolution()
--- problem102/problem.py
+++ problem102/problem.py
@@ -1,25 +1,31 @@
-from mathLib.mathLib import convex
-from time import clock as cl
+def simpleSolution():
+	from mathLib.mathLib import convex
+	f = open("triangles.txt", "r")
 
-f = open("triangles.txt", "r")
+	triangles = []
 
-triangles = []
+	for line in f:
+		# For each line, take off the newline at the end, split on the comma, map all items to 
+		# integers, and then append the created list to triangles.
+		# Complexity: I'm going to assume N
+		triangles.append(map(int,line.strip().split(",")))
 
-for line in f:
-	# For each line, take off the newline at the end, split on the comma, map all items to 
-	# integers, and then append the created list to triangles.
-	# Complexity: I'm going to assume N
-	triangles.append(map(int,line.strip().split(",")))
-startTime = cl()
-trianglesContainingOrigin = 0
+	trianglesContainingOrigin = 0
 
-# Treating this as a convex hull problem (http://en.wikipedia.org/wiki/Convex_hull)
-for triangle in triangles:
-	points = [[triangle[0],triangle[1]],[triangle[2],triangle[3]],[triangle[4],triangle[5]],[0,0]]
+	# Treating this as a convex hull problem (http://en.wikipedia.org/wiki/Convex_hull)
+	for triangle in triangles:
+		points = [[triangle[0],triangle[1]],[triangle[2],triangle[3]],[triangle[4],triangle[5]],[0,0]]
 
-	convexPoints = convex(points)
+		convexPoints = convex(points)
 
-	if convexPoints.count([0,0]) == 0:
-		trianglesContainingOrigin += 1
+		if convexPoints.count([0,0]) == 0:
+			trianglesContainingOrigin += 1
 
-print trianglesContainingOrigin, cl() - startTime
+	return trianglesContainingOrigin
+
+if __name__ == "__main__":
+	import os, sys, inspect
+	cmd_folder = os.sep.join(os.path.abspath(inspect.getfile(inspect.currentframe())).split(os.sep)[:-2])
+	if cmd_folder not in sys.path:
+		sys.path.insert(0, cmd_folder)
+	print simpleSolution()
