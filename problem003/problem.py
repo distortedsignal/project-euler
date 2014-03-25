229995d9b9d5dccaed3f0f1ea4c342f6f0ca190c
Tom Kelley <tkelley@axolotl.com>
2014-03-25 14:37:30 -0700

    Modifying problem 3 to work on Windows.

--- problem003/problem.py
+++ problem003/problem.py
@@ -1,31 +1,32 @@
-from mathLib.mathLib import isPrime as p, factor as f
-from time import clock as c
-from math import sqrt, ceil
-
 bigConst = 600851475143
 
-# startTime = c()
-# #First attempt, not bad, but not optimal (~6.7 seconds)
-# for i in xrange(2,bigConst):
-# 	if bigConst % i == 0:
-# 		if p(int(bigConst/i)):
-# 			print int(bigConst/i)
-# 			break
+#First attempt, not bad, but not optimal (~6.7 seconds)
+def simpleSolution():
+	from mathLib.mathLib import isPrime as p
+	for i in xrange(2,bigConst):
+		if bigConst % i == 0:
+			if p(int(bigConst/i)):
+				return int(bigConst/i)
+				break
 
-# print c() - startTime
 
-startTime = c()
 #Second attempt, better, but still not great (~0.07 seconds)
-for i in range(int(ceil(sqrt(bigConst))),1, -1):
-	if bigConst % i == 0:
-		if p(i):
-			print i
-			break
+def nuancedSolution():
+	from mathLib.mathLib import isPrime as p
+	for i in range(int(ceil(sqrt(bigConst))),1, -1):
+		if bigConst % i == 0:
+			if p(i):
+				return i
 
-print c() - startTime
-
-startTime = c()
 #Optimal solution, given by Project Euler (not my work) and broken out into a different file.
-print f(bigConst)[-1]
+def correctSolution():
+	from mathLib.mathLib import factor as f
+	return f(bigConst)[-1]
 
-print c() - startTime
+if __name__ == "__main__":
+	import os, sys, inspect
+	cmd_folder = os.sep.join(os.path.abspath(inspect.getfile(inspect.currentframe())).split(os.sep)[:-2])
+	if cmd_folder not in sys.path:
+		sys.path.insert(0, cmd_folder)
+	from math import sqrt, ceil
+	print correctSolution()
