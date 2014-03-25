e693393ba223a55f689dfdac5df05fe04cc22808
Tom Kelley <tkelley@axolotl.com>
2014-03-25 13:52:17 -0700

    Making it so problems can work on Windows as well. Also, now I can delete all the symlinks to mathLib.

--- problem002/problem.py
+++ problem002/problem.py
@@ -3,5 +3,9 @@ def simpleSolution():
 	return sum(filter(lambda i: i % 2 == 0, f(4000000)))
 
 if __name__ == "__main__":
+	import os, sys, inspect
+	cmd_folder = os.sep.join(os.path.abspath(inspect.getfile(inspect.currentframe())).split(os.sep)[:-2])
+	if cmd_folder not in sys.path:
+		sys.path.insert(0, cmd_folder)
 	print simpleSolution()
 	
