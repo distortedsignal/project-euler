e4afce62cd6f0dbd15937e12a142f24fb1b8313a
Tom Kelley <tkelley@axolotl.com>
2014-04-04 11:26:49 -0700

    Problem 107 working in about 2 seconds.

--- problem107/problem.py
+++ problem107/problem.py
@@ -3,11 +3,20 @@ class Node:
 		self.name = name
 		self.connectedTo = list([])
 
+	def addLink(self, linkTuple):
+		self.connectedTo.append(linkTuple)
+
+	def removeLink(self, linkTuple):
+		self.connectedTo.remove(linkTuple)
+
 def traverses(start, end, visited):
 	if start == end:
 		return True
+	if len(start.connectedTo) == 0:
+		return False
 
-	for node in start.connectedTo:
+	for link in start.connectedTo:
+		node = link[0]
 		if visited.count(node) == 0:
 			visited.append(node)
 			if traverses(node, end, visited):
@@ -17,16 +26,41 @@ def traverses(start, end, visited):
 def simpleSolution(filePath):
 	# Read in file
 	f = open(filePath, "r")
-	ignoreIndex = 0
+	index = 0
 	nodeList = list([])
+	edgeList = list([])
+	originalSum = 0
 	first = True
 	for line in f:
-		weightList = line.split(",")
+		weightList = map(lambda x: x.strip(), line.split(","))
 		if first:
 			first = False
 			for i in range(len(weightList)):
 				nodeList.append(Node(str(i)))
-		print weightList
+		for i in range(len(weightList)):
+			if weightList[i] != '-':
+				nodeList[index].addLink((nodeList[i],int(weightList[i])))
+				if edgeList.count((i, index, int(weightList[i]))) == 0:
+					edgeList.append((index, i, int(weightList[i])))
+					originalSum += int(weightList[i])
+		index += 1
+
+	edgeList.sort(key = lambda x: -x[2])
+
+	goodEdges = list([])
+
+	for i in edgeList:
+		nodeList[i[0]].removeLink((nodeList[i[1]], i[2]))
+		if traverses(nodeList[i[0]], nodeList[i[1]], [nodeList[i[0]]]):
+			nodeList[i[1]].removeLink((nodeList[i[0]], i[2]))
+		else:
+			nodeList[i[0]].addLink((nodeList[i[1]], i[2]))
+			goodEdges.append(i[2])
+
+
+	print goodEdges
+
+	return originalSum - sum(goodEdges)
 
 if __name__ == "__main__":
 	print simpleSolution("network.txt")
