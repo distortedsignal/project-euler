474004b5ea755c3d8f7d2d33961bdf4e52565af8
Tom Kelley <tkelley@axolotl.com>
2014-09-08 09:24:34 -0700

    Problem 209 done. I should clean up the logging output.

--- problem209/problem.py
+++ problem209/problem.py
@@ -1,3 +1,27 @@
+from time import time
+
+def fibGen(x, y):
+	fibGen.lower, fibGen.higher, fibGen.count = x, y, 2
+	def g():
+		fibGen.lower, fibGen.higher = fibGen.higher, fibGen.lower + fibGen.higher
+		fibGen.count += 1
+		return (fibGen.higher, fibGen.count)
+	return g
+
+def countOptions(digits):
+	if digits == 1:
+		return 1
+
+	if digits == 2:
+		return 3
+
+	gen = fibGen(1,3)
+
+	while True:
+		fibTuple = gen()
+		if fibTuple[1] == digits:
+			return fibTuple[0]
+
 def generateShift(idx):
 	binaryString = '{0:06b}'.format(idx)
 	result = int(binaryString[0]) ^ (int(binaryString[1]) & int(binaryString[2]))
@@ -6,7 +30,7 @@ def generateShift(idx):
 if __name__ == "__main__":
 	generatedMap = {}
 	cycles = []
-
+	t = time()
 	for i in range(64):
 		generatedMap[i] = generateShift(i)
 
@@ -36,6 +60,14 @@ if __name__ == "__main__":
 					
 			cycles.append(newCycle)
 
+	prod = 1
+
+	for i in cycles:
+		prod *= countOptions(len(i))
+
+	print prod
+
 	print cycles
+	print "Time taken:", time() - t
 
 
